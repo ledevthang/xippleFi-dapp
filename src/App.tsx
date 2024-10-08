9fa09f752762f41a93c4f4966c870e74fb7f0093
TonyVu <hocvanvu1999@gmail.com>
2024-10-08 09:25:10 +0700

    feat: init repo, setup code base

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <main>
+      <Button variant="destructive">Hello world</Button>
+    </main>
+  );
+}
+
+export default App;
